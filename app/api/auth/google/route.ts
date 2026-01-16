0c0e06d3a627d95bd22fb98f1ab85bc2e5cefcf8
sudhir-kumar999 <102527512+sudhir-kumar999@users.noreply.github.com>
2026-01-16 18:04:01 +0530

    Update route.ts

--- app/api/auth/google/route.ts
+++ app/api/auth/google/route.ts
@@ -6,9 +6,9 @@ export async function GET() {
 
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
-    // options: {
-    //   redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`,
-    // },
+    options: {
+      redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`,
+    },
   });
 
   if (error) {
